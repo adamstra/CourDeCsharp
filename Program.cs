71b4285cbc224b765c8b5791b1d6571262e4e3c3
Adams tra <liladams97@gmail.com>
2020-07-25 19:06:56 +0000

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+
+namespace CourDeCsharp
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Voiture auto = new Voiture();
+            auto.Klaxonner();
+            auto.VitesseAutorisee(90);
+        }
+
+    }
+}
---
+++ Voiture.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CourDeCsharp
+{
+    class Voiture
+    {
+        public bool VitesseAutorisee(int vitesse)
+        {
+            return vitesse <= 180;
+        }
+
+        public void Klaxonner()
+        {
+            if (!VitesseAutorisee(180))
+                Console.WriteLine("Pouet !");
+            else
+                Console.WriteLine("Danger de mort");
+        }
+    }
+}
